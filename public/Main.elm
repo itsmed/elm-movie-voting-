b94b539b218ef11bfc645396d7832170071ece46
DejonGill <dejonjgill@gmail.com>
2016-07-28 16:36:08 -0700

    Almost have the Http working!

--- public/Main.elm
+++ public/Main.elm
@@ -5,80 +5,71 @@ import Html.Events exposing (onInput, onClick)
 import String exposing(..)
 import Regex exposing (..)
 import Char exposing (isDigit, isLower)
+--import JSON
 
 main =
   App.program
   { init = init
   , view = view
   , update = update
-  , subscriptions = subscriptions
+  --, subscriptions = subscriptions
   }
+
 
 -- MODEL
-type alias Model = 
-  { username : String
-  , password : String
-  , passwordAgain : String
-  , validateForm : Bool
+type alias Model =
+  { topic : String
+  , gifUrl : String
   }
 
-init : Model
-init =
-  Model "" "" "" False
+-- INIT
 
--- SUBSCRIPTIONS
+init : (Model, Cmd Msg)
+init =
+  (Model "cats" "waiting.gif", Cmd.none)
+
+
+-- getRandomGif
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+
+  in
+    Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
+
+decodeGifUrl : Json.Decoder String
+decodeGifUrl =
+  Json.at [ "data", "image_url" ] Json.string
+
 
 -- UPDATE
-type Msg
-  = Name String
-  | Password String
-  | PasswordAgain String
-  | ValidateForm
+type Msg 
+  = MorePlease
+  | FetchSucceed String
+  | FetchFail Http.Error
 
-update : Msg -> Model -> Model
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Name name ->
-      { model | name = name }
-    Password password ->
-      { model | password = password }
-    PasswordAgain passwordAgain ->
-      { model | passwordAgain = passwordAgain }
-    ValidateForm ->
-      { model | validateForm = True }
+    
+    MorePlease ->
+      (model, getRandomGif model.topic)
+
+    FetchSucceed newUrl ->
+      (Model model.topic newUrl, Cmd.none)
+
+    FetchFail _ ->
+      (model, Cmd.none)
 
 
 -- VIEW
 view : Model -> Html Msg
 view model =
   div []
-  [ input [ type' "text", placeholder "Name", onInput Name ] []
-  , input [ type' "password", placeholder "Password", onInput Password ] []
-  , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-  , button [ onClick ValidateForm ] [ text "Submit" ]
-  , validateForm model
-  ]
-
-validateForm : Model -> Html Msg
-validateForm model =
-  let
-    (color, message) =
-      if model.validateForm then
-        if String.length model.password < 8 then 
-          ("red", "Not enough characters in password")
-        else if any isLower model.password == False then
-          ("red", "Password must contain lower case letters")
-        else if model.password == toLower model.password then
-          ("red", "Password must contain uppercase letters")
-        else if any isDigit model.password == False then 
-          ("red", "Passowrd must contain digits")
-        else if model.password /= model.passwordAgain then
-          ("red", "Passwords don't match")
-        else if all isDigit model.age == False then
-          ("red", "Age must be a number")
-        else
-          ("green", "ok")
-      else
-        ("white", "")
-  in 
-    div [ style [("color", color)] ] [ text message ]
+    [ h2 [] [ text model.topic ]
+    , img [ src model.gifUrl ] []
+    , button [ onClick MorePlease ] [ text "More Please" ]
+    ]
